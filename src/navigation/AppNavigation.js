e91e89ba9048d4e24376a7ccdfba64cb016e611b
Saurav Kumar <kumarsaurav26198@gmail.com>
2022-09-21 10:53:31 +0530

    starting stream chat

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Chatscreen, SettingScreen } from '../pages/afterlogin';
@@ -10,11 +10,25 @@ export default function AppNavigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Chatscreen" component={Chatscreen} />
+                <Stack.Screen name="Chatscreen" component={Chatscreen}
+                    options={() => ({
+                        title: 'Chats',
+                    })}
+                />
                 <Stack.Screen name="Setting" component={SettingScreen} />
             </Stack.Navigator>
         </NavigationContainer>
 
     );
 }
-
+const styles = StyleSheet.create({
+    headerTitle: {
+        fontSize: 20
+    },
+    headerTitleStyle: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        alignSelf: 'center',
+        color: 'black',
+    },
+});
